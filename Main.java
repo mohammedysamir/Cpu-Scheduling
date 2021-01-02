97d10d121315591416c1e53831e033586c6478e2
mohammed yasser <mohammedre4a@gmail.com>
2021-01-02 23:04:13 +0000

    Feat : Create process class with common 'changing' attributes , Create simple main to take inputs and creating arraylist of processes.

--- Main.java
+++ Main.java
@@ -1,5 +1,38 @@
+import java.util.*;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Scanner scan=new Scanner(System.in);
+    int Burst,Arrival,ContextSwitching,Priority,Quantum,Queue_num,ProcessNumbers;
+    String Name;
+    
+    ArrayList<Process> Processes=new ArrayList<Process>();
+
+    System.out.print("Enter number of processes: ");
+    ProcessNumbers=scan.nextInt();
+    for(int i=0;i<ProcessNumbers;i++)
+    {
+    scan.nextLine();
+    
+    System.out.print("Enter Name of processes: ");
+    Name=scan.nextLine(); 
+    System.out.print("Enter Arrival Time of processes: ");
+    Arrival=scan.nextInt();
+    System.out.print("Enter Burst Time of processes: ");
+    Burst=scan.nextInt();
+    System.out.print("Enter Priority of processes: ");
+    Priority=scan.nextInt();
+    System.out.print("Enter Queue number of processes: ");
+    Queue_num=scan.nextInt();
+    System.out.println("-------------------------------------------------");
+    Process p=new Process(Name,Arrival,Burst,Priority,Queue_num);//create process
+    Processes.add(p);//add process to arraylist
+    }
+    System.out.print("Enter Context Switching time: ");
+    ContextSwitching=scan.nextInt();
+    System.out.print("Enter Quantum Time: ");
+    Quantum=scan.nextInt();
+
   }
+  
 }
---
+++ Process.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Process{
+String Name;
+int ArrivalTime,BurstTime,Priority,QueueNumber;
+Process(String N,int A,int B,int P,int Q)
+{
+  Name=N;
+  ArrivalTime=A;
+  BurstTime=B;
+  Priority=P;
+  QueueNumber=Q;
+}
+
+
+
+}
