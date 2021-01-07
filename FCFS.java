1487f3420b312a4110e4749f4470007ff33eaa99
Mohammed Yasser <mohammedre4a@gmail.com>
2021-01-07 02:43:50 +0200

    refactor : add temp arraylist to store processes after assigning TA time and waiting time to it.

--- FCFS.java
+++ FCFS.java
@@ -9,38 +9,39 @@ public class FCFS{
 
   public void Schedule(){
     int counter=0;    //refer to current time
-    //create temp ArrayList
-     ArrayList<Process> temp=process;
+    //create Result ArrayList to store result
+    ArrayList<Process> Result=new ArrayList<Process>(); 
     while(!process.isEmpty()){
       int index=get_minArrival();
-      int burst=temp.get(index).BurstTime;
+      int burst=process.get(index).BurstTime;
       while(burst > 0){//having some time to do process
         counter++;
         burst--;
       }
       //assign Turnaround time
-      process.get(index).Turnaround=counter-temp.get(index).ArrivalTime;
+      process.get(index).Turnaround=counter-process.get(index).ArrivalTime;
       //assign Waiting time
-      process.get(index).Waiting=temp.get(index).Turnaround-temp.get(index).BurstTime; 
+      process.get(index).Waiting=process.get(index).Turnaround-process.get(index).BurstTime; 
       //Print data of Process
       process.get(index).ShowInfo();
       //remove from Queue
-       temp.remove(index);
+      Result.add(process.get(index));
+      process.remove(index);
     }
-    GetAvrg();
+    GetAvrg(Result);
   }
 
-  public void GetAvrg(){
+  public void GetAvrg(ArrayList<Process> R){
     float totalTurnarround=0.0f;
     float totalWaiting=0.0f;
-    for(int i=0;i<process.size();i++)
+    for(int i=0;i<R.size();i++)
     {
-      totalTurnarround+=process.get(i).Turnaround;
-      totalWaiting+=process.get(i).Waiting;
+      totalTurnarround+=R.get(i).Turnaround;
+      totalWaiting+=R.get(i).Waiting;
     }
 
-    float TurnaroundAvg=totalTurnarround/(float)process.size();
-    float WaitingAvg=totalWaiting/(float)process.size();
+    float TurnaroundAvg=totalTurnarround/(float)R.size();
+    float WaitingAvg=totalWaiting/(float)R.size();
     
     System.out.println("in First-Come-First-Served, Average Turn Arround Time: "+TurnaroundAvg+" "+ "Average Waiting Time: "+WaitingAvg );
   }
